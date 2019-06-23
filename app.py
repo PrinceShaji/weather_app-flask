d1ea2fe46cf525f4632959e35ff4841ced2bc5cf
PrinceShaji <22243569+PrinceShaji@users.noreply.github.com>
2019-06-23 13:41:48 +0530

    Added 404 page.

--- app.py
+++ app.py
@@ -3,19 +3,19 @@
 ''' Main app for the flask weather app. '''
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepahe():
-    return render_template('index.html')
-
-
-
-
-
-
+@app.route('/', methods = ['GET', 'POST'])
+def homepage():
+    if request.method == "POST":
+        return render_template('error.html')
+        #Get city method
+        #If true, get the data for city
+        #return the new page
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
